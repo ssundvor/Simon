97c910c0c1eac7c2fd4b17241c4fc49e07a487c9
Scott Sundvor <ssundvor@gmail.com>
2013-02-27 21:19:17 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simon::Application.config.secret_token = 'a7a51f2d2cd1c1f79f07fa21b24a772e39a72489f5d886eaf6a0f145a7f07e80359b32f13bd1223adf5f4d702993b7da28e498447de782924fbcdbba9f58e8ca'
